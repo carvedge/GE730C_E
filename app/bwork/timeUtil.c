2f5b1dd7e70a00852394133bf632a71021214d12
yaoyanlianglove <yaoyanliang.love@hotmail.com>
2017-07-13 16:49:34 +0800

    V2.5.1.170712_beta_k

--- app/bwork/timeUtil.c
+++ app/bwork/timeUtil.c
@@ -21,7 +21,7 @@ char * get_TimeDetail(){
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
 	char* ret = (char*)malloc(100);
-	sprintf(ret,"%d%02d%02d%02d%02d",tm.tm_year + 1900, tm.tm_mon+1,tm.tm_mday,tm.tm_hour,tm.tm_min);
+	sprintf(ret,"%d%02d%02d%02d%02d%02d",tm.tm_year + 1900, tm.tm_mon+1,tm.tm_mday,tm.tm_hour,tm.tm_min,tm.tm_sec);
 	return ret;
 }
 long getTImeInterVal(){
